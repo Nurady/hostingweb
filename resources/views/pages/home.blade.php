647898052017c230c24cf4239c3302ea7430b0b3
my office project <vueapp2022@gmail.com>
2023-12-07 01:43:35 +0800

    add iklan

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -536,6 +536,23 @@
         </div>
     </div>
 
+    <div class="iklan container-xxl py-5">
+        <div class="container">
+            <div class="card p-5 border-0">
+                <div class="row align-items-center">
+                    <div class="col-md-4 mb-2">
+                        <img class="w-100" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/illustrations/bw.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/illustrations/bw.webp">
+                    </div>
+                    <div class="col-md-8 mb-2">
+                        <h3><span>Bikin website impian</span> dengan mudah disini hanya 1jutaan</h3>
+                        <p>Bikin website impian include paket hosting Indonesia terbaik hanya 1 jutaan Jasa pembuatan website profesional untuk Perusahaan, Bisnis & UKM. Bikin Website Impian dengan mudah di sini, desain website elegan dan mudah dikelola</p>
+                        <button class="btn btn-primary">Lihat Selengkapnya</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <!-- About Start -->
     {{-- <div class="container-xxl py-5">
         <div class="container px-lg-5">
