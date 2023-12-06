28355441e560be530c4eafc1b633c2f7cacce04c
my office project <vueapp2022@gmail.com>
2023-12-06 19:59:55 +0800

    update tampilan hero dan nav

--- resources/views/includes/hero.blade.php
+++ resources/views/includes/hero.blade.php
@@ -2,16 +2,22 @@
     <div class="container my-5 py-5 px-lg-5">
         <div class="row g-5">
             <div class="col-lg-6 pt-5 text-center text-lg-start">
-                <h1 class="display-4 text-white mb-4 animated slideInLeft">Shared Hosting</h1>
-                <p class="text-white animated slideInLeft">Tempor rebum no at dolore lorem clita rebum rebum ipsum rebum stet dolor sed justo kasd.</p>
-                <h1 class="text-white mb-4 animated slideInLeft">
-                    <small class="align-top fw-normal" style="font-size: 15px; line-height: 25px;">Starting:</small>
-                    <span>$2.49</span>
-                    <small class="align-bottom fw-normal" style="font-size: 15px; line-height: 33px;">/ Mo</small>
+                <h1 class="display-4 text-white mb-4 animated slideInLeft main-title text-center">
+                    Cloud Hosting untuk <span>Kesuksesan Websitemu!</span>
                 </h1>
-                <a href="#" class="btn-hero btn btn-secondary py-sm-3 px-sm-5 me-3 animated slideInLeft">Get Started Now</a>
+                <p class="text-white animated slideInLeft text-center">
+                    Qwords menawarkan layanan Cloud Hosting yang Cepat, Aman, dan Dukungan 24 jam untuk memenuhi kebutuhan hosting Anda. Tersertifikasi ISO 27001 dengan Data Center Tier 3.
+                </p>
+                <h1 class="text-white mb-4 animated slideInLeft text-center">
+                    <small class="align-bottom fw-normal" style="font-size: 25px; line-height: 33px; font-weight: 800;">#QwordsYourSuccess</small>
+                </h1>
+                <div class="text-center">
+                    <a href="#" class="btn-hero btn btn-secondary py-sm-3 px-sm-5 me-3 animated slideInLeft">
+                        Pesan Sekarang
+                    </a>
+                </div>
             </div>
-            <div class="col-lg-6 text-center text-lg-start">
+            <div class="col-lg-6 text-center">
                 <img class="img-fluid animated zoomIn" src="{{ asset('frontend/img/hero.png') }}" alt="">
             </div>
         </div>
--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
-    <a href="" class="navbar-brand p-0">
+    <a href="/" class="navbar-brand p-0">
         <img src="{{ asset('frontend/img/logo-qw-light.webp') }}" alt="logo-qw-light.webp">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
