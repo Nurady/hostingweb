ba732df7cb99afcaa471e68db0fbfbb424b8ad4f
my office project <vueapp2022@gmail.com>
2023-12-07 07:29:10 +0800

    add quarantee

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -622,6 +622,20 @@
         </div>
     </div>
 
+    <div class="mt-5 quarantee p-5">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <img class="w-100" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/illustrations/guarantee.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/illustrations/guarantee.webp">
+                </div>
+                <div class="col-md-6">
+                    <h3>Garansi 30 Hari <span>Uang Kembali</span></h3>
+                    <p>Untuk menjaga kepuasan pelanggan, kami memberikan garansi uang kembali yang berlaku hingga 30 hari setelah hosting aktif.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <!-- About Start -->
     {{-- <div class="container-xxl py-5">
         <div class="container px-lg-5">
