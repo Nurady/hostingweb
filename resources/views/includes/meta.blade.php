efaaa9b8ae5f06255cdc994be044a529c36a24fd
my office project <vueapp2022@gmail.com>
2023-12-06 17:31:33 +0800

    frontend templating

---
+++ resources/views/includes/meta.blade.php
@@ -0,0 +1,7 @@
+<meta charset="utf-8">
+<title>@yield('title')</title>
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
+<meta content="" name="keywords">
+<meta content="" name="description">
+
+<link href="{{ asset('favicon.ico') }}" rel="icon">
