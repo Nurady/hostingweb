d54abe6e3b7c01fa841868737b42f838312fccbd
my office project <vueapp2022@gmail.com>
2023-12-07 01:28:31 +0800

    add solution dan komitmen

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -59,47 +59,6 @@
     </div>
     <!-- Domain End -->
 
-    <!-- About Start -->
-    {{-- <div class="container-xxl py-5">
-        <div class="container px-lg-5">
-            <div class="row g-5 align-items-center">
-                <div class="col-lg-7 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="section-title position-relative mb-4 pb-4">
-                        <h1 class="mb-2">Welcome to GreenHost</h1>
-                    </div>
-                    <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum et tempor sit. Aliqu diam amet diam et eos labore. Clita erat ipsum et lorem et sit, sed stet no labore lorem sit clita duo justo magna dolore erat amet</p>
-                    <div class="row g-3">
-                        <div class="col-sm-4 wow fadeIn" data-wow-delay="0.1s">
-                            <div class="bg-light rounded text-center p-4">
-                                <i class="fa fa-users-cog fa-2x text-primary mb-2"></i>
-                                <h2 class="mb-1" data-toggle="counter-up">1234</h2>
-                                <p class="mb-0">Experts</p>
-                            </div>
-                        </div>
-                        <div class="col-sm-4 wow fadeIn" data-wow-delay="0.3s">
-                            <div class="bg-light rounded text-center p-4">
-                                <i class="fa fa-users fa-2x text-primary mb-2"></i>
-                                <h2 class="mb-1" data-toggle="counter-up">1234</h2>
-                                <p class="mb-0">Clients</p>
-                            </div>
-                        </div>
-                        <div class="col-sm-4 wow fadeIn" data-wow-delay="0.5s">
-                            <div class="bg-light rounded text-center p-4">
-                                <i class="fa fa-check fa-2x text-primary mb-2"></i>
-                                <h2 class="mb-1" data-toggle="counter-up">1234</h2>
-                                <p class="mb-0">Projects</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-5">
-                    <img class="img-fluid wow zoomIn" data-wow-delay="0.5s" src="{{ asset('frontend/img/about.png') }}">
-                </div>
-            </div>
-        </div>
-    </div> --}}
-    <!-- About End -->
-
     <div class="service container-xxl py-5">
         <div class="container px-lg-5">
             <div class="section-title position-relative text-center mx-auto mb-5 pb-4 wow fadeInUp" data-wow-delay="0.1s">
@@ -406,6 +365,217 @@
             </div>
         </div>
     </div>
+
+    <div class="solution container-xxl py-5">
+        <div class="container px-lg-5">
+            <div class="section-title position-relative text-center mx-auto mb-5 pb-4 wow fadeInUp" data-wow-delay="0.1s">
+                <h1 class="mb-3">
+                    Solusi Paket Hosting Terbaik dari Kami
+                </h1>
+                <p class="mb-1">
+                    Kami menawarkan solusi paket hosting terbaik untuk segala kebutuhan website dan sistem Anda. 
+                    <br>
+                    Mulai dari blog hingga website bisnis dan perusahaan.
+                </p>
+            </div>
+            <div class="row mt-3">
+                <div class="col-md-4 mb-3">
+                    <a href="https://qwords.com/v2/hosting-dan-server-perusahaan/" style="text-decoration: none;">
+                        <div class="card border-0 p-3">                            
+                            <h3>Perusahaan</h3>
+                            <p>
+                                Wujudkan website perusahaan profesional dan berkelas dengan dukungan web hosting terbaik dan fitur keamanan ekstra
+                            </p>                            
+                            <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/company.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/company.webp">
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <a href="https://qwords.com/v2/hosting-dan-server-perusahaan/" style="text-decoration: none;">
+                        <div class="card border-0 p-3">                            
+                            <h3>UMKM dan Toko Online</h3>
+                            <p>
+								Memberikan hosting terbaik untuk mendukung kebutuhan website pemasaran barang/jasa secara online.
+                            </p>                            
+                            <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/umkm.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/umkm.webp">
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <a href="https://qwords.com/v2/hosting-dan-server-perusahaan/" style="text-decoration: none;">
+                        <div class="card border-0 p-3">                            
+                            <h3>Organisasi dan Komunitas</h3>
+                            <p>
+								Bikin website organisasi atau komunitas Anda dengan paket hosting unggulan yang ramah kantong.
+                            </p>                            
+                            <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/orgs.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/orgs.webp">
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <a href="https://qwords.com/v2/hosting-dan-server-perusahaan/" style="text-decoration: none;">
+                        <div class="card border-0 p-3">                            
+                            <h3>Sekolah</h3>
+                            <p>
+								Infrastruktur web hosting terbaik untuk menunjang kegiatan e-learning. Didukung platform MOODLE khusus pembelajaran daring.
+                            </p>                            
+                            <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/school.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/school.webp">
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <a href="https://qwords.com/v2/hosting-dan-server-perusahaan/" style="text-decoration: none;">
+                        <div class="card border-0 p-3">                            
+                            <h3>Developer</h3>
+                            <p>
+								Tersedia paket hosting Indonesia dengan spesifikasi khusus yang dev-friendly. Ekstra NodeJS dan Git Version Control.
+                            </p>                            
+                            <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/dev.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/dev.webp">
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <a href="https://qwords.com/v2/hosting-dan-server-perusahaan/" style="text-decoration: none;">
+                        <div class="card border-0 p-3">                            
+                            <h3>Blogger dan Personal</h3>
+                            <p>
+								TMemfasilitasi blogger dengan infrastruktur hosting Indonesia terbaik serta template website premium untuk tampil lebih profesional.
+                            </p>                            
+                            <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/blog.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/blog.webp">
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="commitment container-xxl py-5">
+        <div class="container px-lg-5">
+            <div class="section-title position-relative text-center mx-auto mb-5 pb-4 wow fadeInUp" data-wow-delay="0.1s">
+                <h1 class="mb-3">
+                    Komitmen Qwords
+                </h1>
+                <p class="mb-1">
+                    Berbagai alasan mengapa pelanggan memilih sewa hosting di Qwords. 
+                </p>
+            </div>
+            <div class="row justify-content-center mt-3">
+                <div class="col-md-4 mb-3">
+                    <div class="card border-0 p-3">
+                        <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/call.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/call.webp">                            
+                        <h3 class="mt-3">Layanan 24/7</h3>
+                        <p>
+                            Tim Technical Support dan Customer Service Qwords siap membantu Anda selama 24 jam, 7 hari. Anda dapat menghubungi melalui Live chat, Call Center, dan Support Ticket.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card border-0 p-3">
+                        <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/book.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/book.webp">                            
+                        <h3 class="mt-3">Panduan Manual Lengkap</h3>
+                        <p>
+                            Qwords dilengkapi dengan halaman knowledge base berisi tutorial dan tips seputar pengelolaan website dan hosting. Dapat diakses dengan mudah dan solutif.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card border-0 p-3">
+                        <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/router.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/router.webp">                            
+                        <h3 class="mt-3">Up time 99.99%</h3>
+                        <p>
+                            Uptime Network & Server di atas 99. 99% dengan konfigurasi server yang tepat. Didukung manajemen jaringan dan multiple upstream tier 1 provider network serta multiple peering.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card border-0 p-3">
+                        <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/guard.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/guard.webp">                            
+                        <h3 class="mt-3">Jaminan keamanan</h3>
+                        <p>
+                            Keamanan menjadi fokus utama kami di Qwords. Tindakan pemantauan dan pencegahan yang berstandar & berkelanjutan telah menjadi perhatian utama kami.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card border-0 p-3">
+                        <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/networkAlt.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/networkAlt.webp">                            
+                        <h3 class="mt-3">Clustered DNS</h3>
+                        <p>
+                            DNS Server Cloud Hosting Qwords terhubung satu dengan yang lainnya. Hal ini membuat DNS Server akan selalu dapat di akses karena saling membackup antar server.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card border-0 p-3">
+                        <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/diamond.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/diamond.webp">                            
+                        <h3 class="mt-3">High Enterprise Server</h3>
+                        <p>
+                            Standar server Qwords adalah server kelas enterprise Dual Xeon Octa Core yang merupakan hardware kelas terbaik. Hardware ini biasa digunakan oleh perusahaan terkemuka.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card border-0 p-3">
+                        <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/network.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/network.webp">                            
+                        <h3 class="mt-3">Peering Network</h3>
+                        <p>
+                            Jaringan di Qwords terhubung langsung ke public peering seperti OpenIXP dan CDIX yang membuat latency atau waktu tunggu pengiriman data semakin kecil.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card border-0 p-3">
+                        <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/router.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/router.webp">                            
+                        <h3 class="mt-3">Tier 1 Network</h3>
+                        <p>
+                            Tier 1 Network merupakan jaringan dunia tanpa transit network, yang membuat jaringan lebih cepat serta jalur routing yang lebih pendek dan ketersediaan backup network.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- About Start -->
+    {{-- <div class="container-xxl py-5">
+        <div class="container px-lg-5">
+            <div class="row g-5 align-items-center">
+                <div class="col-lg-7 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="section-title position-relative mb-4 pb-4">
+                        <h1 class="mb-2">Welcome to GreenHost</h1>
+                    </div>
+                    <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum et tempor sit. Aliqu diam amet diam et eos labore. Clita erat ipsum et lorem et sit, sed stet no labore lorem sit clita duo justo magna dolore erat amet</p>
+                    <div class="row g-3">
+                        <div class="col-sm-4 wow fadeIn" data-wow-delay="0.1s">
+                            <div class="bg-light rounded text-center p-4">
+                                <i class="fa fa-users-cog fa-2x text-primary mb-2"></i>
+                                <h2 class="mb-1" data-toggle="counter-up">1234</h2>
+                                <p class="mb-0">Experts</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-4 wow fadeIn" data-wow-delay="0.3s">
+                            <div class="bg-light rounded text-center p-4">
+                                <i class="fa fa-users fa-2x text-primary mb-2"></i>
+                                <h2 class="mb-1" data-toggle="counter-up">1234</h2>
+                                <p class="mb-0">Clients</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-4 wow fadeIn" data-wow-delay="0.5s">
+                            <div class="bg-light rounded text-center p-4">
+                                <i class="fa fa-check fa-2x text-primary mb-2"></i>
+                                <h2 class="mb-1" data-toggle="counter-up">1234</h2>
+                                <p class="mb-0">Projects</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5">
+                    <img class="img-fluid wow zoomIn" data-wow-delay="0.5s" src="{{ asset('frontend/img/about.png') }}">
+                </div>
+            </div>
+        </div>
+    </div> --}}
+    <!-- About End -->
 
     {{-- <div class="container-xxl py-5">
         <div class="container px-lg-5">
