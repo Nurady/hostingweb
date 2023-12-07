c8e30e4b76a78736c72455a3b5c82f8c1b3e4dd3
my office project <vueapp2022@gmail.com>
2023-12-07 12:33:45 +0800

    update team member

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -847,182 +847,11 @@
         </div>
     </div>
 
-    <!-- About Start -->
-    {{-- <div class="container-xxl py-5">
+    <!-- Team Member -->
+    <div class="container-xxl py-5 mt-3">
         <div class="container px-lg-5">
-            <div class="row g-5 align-items-center">
-                <div class="col-lg-7 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="section-title position-relative mb-4 pb-4">
-                        <h1 class="mb-2">Welcome to GreenHost</h1>
-                    </div>
-                    <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum et tempor sit. Aliqu diam amet diam et eos labore. Clita erat ipsum et lorem et sit, sed stet no labore lorem sit clita duo justo magna dolore erat amet</p>
-                    <div class="row g-3">
-                        <div class="col-sm-4 wow fadeIn" data-wow-delay="0.1s">
-                            <div class="bg-light rounded text-center p-4">
-                                <i class="fa fa-users-cog fa-2x text-primary mb-2"></i>
-                                <h2 class="mb-1" data-toggle="counter-up">1234</h2>
-                                <p class="mb-0">Experts</p>
-                            </div>
-                        </div>
-                        <div class="col-sm-4 wow fadeIn" data-wow-delay="0.3s">
-                            <div class="bg-light rounded text-center p-4">
-                                <i class="fa fa-users fa-2x text-primary mb-2"></i>
-                                <h2 class="mb-1" data-toggle="counter-up">1234</h2>
-                                <p class="mb-0">Clients</p>
-                            </div>
-                        </div>
-                        <div class="col-sm-4 wow fadeIn" data-wow-delay="0.5s">
-                            <div class="bg-light rounded text-center p-4">
-                                <i class="fa fa-check fa-2x text-primary mb-2"></i>
-                                <h2 class="mb-1" data-toggle="counter-up">1234</h2>
-                                <p class="mb-0">Projects</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-5">
-                    <img class="img-fluid wow zoomIn" data-wow-delay="0.5s" src="{{ asset('frontend/img/about.png') }}">
-                </div>
-            </div>
-        </div>
-    </div> --}}
-    <!-- About End -->
-
-    {{-- <div class="container-xxl py-5">
-        <div class="container px-lg-5">
-            <div class="section-title position-relative text-center mx-auto mb-5 pb-4 wow fadeInUp" data-wow-delay="0.1s">
-                <h1 class="mb-3">Lihat Juga Layanan Kami Yang Lain</h1>
-                <p class="mb-1">Optimalkan performa website bisnis Anda dengan berbagai layanan berkualitas dari Qwords.</p>
-            </div>
-            <div class="row g-5 comparison position-relative">
-                <div class="col-lg-6 pe-lg-5">
-                    <div class="section-title position-relative mx-auto mb-4 pb-4">
-                        <h3 class="fw-bold mb-0">Shared Server</h3>
-                    </div>
-                    <div class="row gy-3 gx-5">
-                        <div class="col-sm-6 wow fadeIn" data-wow-delay="0.1s">
-                            <i class="fa fa-server fa-3x text-primary mb-3"></i>
-                            <h5 class="fw-bold">99.99% Uptime</h5>
-                            <p>Ipsum dolor diam stet stet kasd diam sea stet sed rebum dolor ipsum</p>
-                        </div>
-                        <div class="col-sm-6 wow fadeIn" data-wow-delay="0.3s">
-                            <i class="fa fa-shield-alt fa-3x text-primary mb-3"></i>
-                            <h5 class="fw-bold">100% Secured</h5>
-                            <p>Ipsum dolor diam stet stet kasd diam sea stet sed rebum dolor ipsum</p>
-                        </div>
-                        <div class="col-sm-6 wow fadeIn" data-wow-delay="0.5s">
-                            <i class="fa fa-cog fa-3x text-primary mb-3"></i>
-                            <h5 class="fw-bold">Control Panel</h5>
-                            <p>Ipsum dolor diam stet stet kasd diam sea stet sed rebum dolor ipsum</p>
-                        </div>
-                        <div class="col-sm-6 wow fadeIn" data-wow-delay="0.7s">
-                            <i class="fa fa-headset fa-3x text-primary mb-3"></i>
-                            <h5 class="fw-bold">24/7 Support</h5>
-                            <p>Ipsum dolor diam stet stet kasd diam sea stet sed rebum dolor ipsum</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 ps-lg-5">
-                    <div class="section-title position-relative mx-auto mb-4 pb-4">
-                        <h3 class="fw-bold mb-0">Dedicated Server</h3>
-                    </div>
-                    <div class="row gy-3 gx-5">
-                        <div class="col-sm-6 wow fadeIn" data-wow-delay="0.1s">
-                            <i class="fa fa-server fa-3x text-secondary mb-3"></i>
-                            <h5 class="fw-bold">99.99% Uptime</h5>
-                            <p>Ipsum dolor diam stet stet kasd diam sea stet sed rebum dolor ipsum</p>
-                        </div>
-                        <div class="col-sm-6 wow fadeIn" data-wow-delay="0.3s">
-                            <i class="fa fa-shield-alt fa-3x text-secondary mb-3"></i>
-                            <h5 class="fw-bold">100% Secured</h5>
-                            <p>Ipsum dolor diam stet stet kasd diam sea stet sed rebum dolor ipsum</p>
-                        </div>
-                        <div class="col-sm-6 wow fadeIn" data-wow-delay="0.5s">
-                            <i class="fa fa-cog fa-3x text-secondary mb-3"></i>
-                            <h5 class="fw-bold">Control Panel</h5>
-                            <p>Ipsum dolor diam stet stet kasd diam sea stet sed rebum dolor ipsum</p>
-                        </div>
-                        <div class="col-sm-6 wow fadeIn" data-wow-delay="0.7s">
-                            <i class="fa fa-headset fa-3x text-secondary mb-3"></i>
-                            <h5 class="fw-bold">24/7 Support</h5>
-                            <p>Ipsum dolor diam stet stet kasd diam sea stet sed rebum dolor ipsum</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> --}}
-
-
-    <!-- Testimonial Start -->
-    <div class="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
-        <div class="container px-lg-5">
-            <div class="owl-carousel testimonial-carousel">
-                <div class="testimonial-item position-relative bg-light border-top border-5 border-primary rounded p-4 my-4">
-                    <div class="d-flex align-items-center justify-content-center position-absolute top-0 start-0 ms-5 translate-middle bg-primary rounded-circle" style="width: 45px; height: 45px; margin-top: -3px;">
-                        <i class="fa fa-quote-left text-white"></i>
-                    </div>
-                    <p class="mt-3">Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</p>
-                    <div class="d-flex align-items-center">
-                        <img class="img-fluid flex-shrink-0 rounded-circle" src="{{ asset('frontend/img/testimonial-1.jpg') }}" style="width: 50px; height: 50px;">
-                        <div class="ps-3">
-                            <h6 class="fw-bold mb-1">Client Name</h6>
-                            <small>Profession</small>
-                        </div>
-                    </div>
-                </div>
-                <div class="testimonial-item position-relative bg-light border-top border-5 border-primary rounded p-4 my-4">
-                    <div class="d-flex align-items-center justify-content-center position-absolute top-0 start-0 ms-5 translate-middle bg-primary rounded-circle" style="width: 45px; height: 45px; margin-top: -3px;">
-                        <i class="fa fa-quote-left text-white"></i>
-                    </div>
-                    <p class="mt-3">Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</p>
-                    <div class="d-flex align-items-center">
-                        <img class="img-fluid flex-shrink-0 rounded-circle" src="{{ asset('frontend/img/testimonial-2.jpg') }}" style="width: 50px; height: 50px;">
-                        <div class="ps-3">
-                            <h6 class="fw-bold mb-1">Client Name</h6>
-                            <small>Profession</small>
-                        </div>
-                    </div>
-                </div>
-                <div class="testimonial-item position-relative bg-light border-top border-5 border-primary rounded p-4 my-4">
-                    <div class="d-flex align-items-center justify-content-center position-absolute top-0 start-0 ms-5 translate-middle bg-primary rounded-circle" style="width: 45px; height: 45px; margin-top: -3px;">
-                        <i class="fa fa-quote-left text-white"></i>
-                    </div>
-                    <p class="mt-3">Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</p>
-                    <div class="d-flex align-items-center">
-                        <img class="img-fluid flex-shrink-0 rounded-circle" src="{{ asset('frontend/img/testimonial-3.jpg') }}" style="width: 50px; height: 50px;">
-                        <div class="ps-3">
-                            <h6 class="fw-bold mb-1">Client Name</h6>
-                            <small>Profession</small>
-                        </div>
-                    </div>
-                </div>
-                <div class="testimonial-item position-relative bg-light border-top border-5 border-primary rounded p-4 mt-4">
-                    <div class="d-flex align-items-center justify-content-center position-absolute top-0 start-0 ms-5 translate-middle bg-primary rounded-circle" style="width: 45px; height: 45px; margin-top: -3px;">
-                        <i class="fa fa-quote-left text-white"></i>
-                    </div>
-                    <p class="mt-3">Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</p>
-                    <div class="d-flex align-items-center">
-                        <img class="img-fluid flex-shrink-0 rounded-circle" src="{{ asset('frontend/img/testimonial-4.jpg') }}" style="width: 50px; height: 50px;">
-                        <div class="ps-3">
-                            <h6 class="fw-bold mb-1">Client Name</h6>
-                            <small>Profession</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Testimonial End -->
-
-
-    <!-- Team Start -->
-    <div class="container-xxl py-5">
-        <div class="container px-lg-5">
-            <div class="section-title position-relative text-center mx-auto mb-5 pb-4 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+            <div class="position-relative text-center mx-auto mb-2 pb-4 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
                 <h1 class="mb-3">Our Team Members</h1>
-                <p class="mb-1">Vero justo sed sed vero clita amet. Et justo vero sea diam elitr amet ipsum eos
-                    ipsum clita duo sed. Sed vero sea diam erat vero elitr sit clita.</p>
             </div>
             <div class="row g-4">
                 <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
@@ -1030,12 +859,12 @@
                         <div class="text-center p-4">
                             <img class="img-fluid rounded-circle mb-4" src="{{ asset('frontend/img/team-1.jpg') }}" alt="">
                             <h5 class="fw-bold mb-1">Full Name</h5>
-                            <small>Designation</small>
+                            <small>Director</small>
                         </div>
                         <div class="d-flex justify-content-center bg-primary p-3">
-                            <a class="btn btn-square text-primary bg-white m-1" href=""><i class="fab fa-facebook-f"></i></a>
-                            <a class="btn btn-square text-primary bg-white m-1" href=""><i class="fab fa-twitter"></i></a>
-                            <a class="btn btn-square text-primary bg-white m-1" href=""><i class="fab fa-instagram"></i></a>
+                            <a class="btn btn-square text-primary bg-white m-1" href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-square text-primary bg-white m-1" href="#"><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-square text-primary bg-white m-1" href="#"><i class="fab fa-instagram"></i></a>
                         </div>
                     </div>
                 </div>
@@ -1044,12 +873,12 @@
                         <div class="text-center p-4">
                             <img class="img-fluid rounded-circle mb-4" src="{{ asset('frontend/img/team-2.jpg') }}" alt="">
                             <h5 class="fw-bold mb-1">Full Name</h5>
-                            <small>Designation</small>
+                            <small>System Analyst</small>
                         </div>
                         <div class="d-flex justify-content-center bg-primary p-3">
-                            <a class="btn btn-square text-primary bg-white m-1" href=""><i class="fab fa-facebook-f"></i></a>
-                            <a class="btn btn-square text-primary bg-white m-1" href=""><i class="fab fa-twitter"></i></a>
-                            <a class="btn btn-square text-primary bg-white m-1" href=""><i class="fab fa-instagram"></i></a>
+                            <a class="btn btn-square text-primary bg-white m-1" href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-square text-primary bg-white m-1" href="#"><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-square text-primary bg-white m-1" href="#"><i class="fab fa-instagram"></i></a>
                         </div>
                     </div>
                 </div>
@@ -1058,12 +887,12 @@
                         <div class="text-center p-4">
                             <img class="img-fluid rounded-circle mb-4" src="{{ asset('frontend/img/team-3.jpg') }}" alt="">
                             <h5 class="fw-bold mb-1">Full Name</h5>
-                            <small>Designation</small>
+                            <small>Full Stack Developer</small>
                         </div>
                         <div class="d-flex justify-content-center bg-primary p-3">
-                            <a class="btn btn-square text-primary bg-white m-1" href=""><i class="fab fa-facebook-f"></i></a>
-                            <a class="btn btn-square text-primary bg-white m-1" href=""><i class="fab fa-twitter"></i></a>
-                            <a class="btn btn-square text-primary bg-white m-1" href=""><i class="fab fa-instagram"></i></a>
+                            <a class="btn btn-square text-primary bg-white m-1" href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-square text-primary bg-white m-1" href="#"><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-square text-primary bg-white m-1" href="#"><i class="fab fa-instagram"></i></a>
                         </div>
                     </div>
                 </div>
@@ -1072,17 +901,16 @@
                         <div class="text-center p-4">
                             <img class="img-fluid rounded-circle mb-4" src="{{ asset('frontend/img/team-4.jpg') }}" alt="">
                             <h5 class="fw-bold mb-1">Full Name</h5>
-                            <small>Designation</small>
+                            <small>IT Infrastructure</small>
                         </div>
                         <div class="d-flex justify-content-center bg-primary p-3">
-                            <a class="btn btn-square text-primary bg-white m-1" href=""><i class="fab fa-facebook-f"></i></a>
-                            <a class="btn btn-square text-primary bg-white m-1" href=""><i class="fab fa-twitter"></i></a>
-                            <a class="btn btn-square text-primary bg-white m-1" href=""><i class="fab fa-instagram"></i></a>
+                            <a class="btn btn-square text-primary bg-white m-1" href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-square text-primary bg-white m-1" href="#"><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-square text-primary bg-white m-1" href="#"><i class="fab fa-instagram"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- Team End -->
 @endsection
