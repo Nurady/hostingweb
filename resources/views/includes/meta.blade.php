932ef737c712bfa1664a1c72ea55069dae04df6a
my office project <vueapp2022@gmail.com>
2023-12-07 19:57:12 +0800

    update meta rel icon link

--- resources/views/includes/meta.blade.php
+++ resources/views/includes/meta.blade.php
@@ -6,4 +6,8 @@
 <meta name="keywords" content="Cloud hosting terbaik, hosting cepat, sewa hosting, beli domain dan hosting, beli domain hosting, hosting, cloud hosting murah, jasa web hosting, cloud hosting, webhosting domain hosting, domain dan hosting, hosting indonesia, hosting dan domain murah, hosting domain, cloud hosting indonesia terbaik, web hosting indonesia, cloud hosting indonesia, webhost app, hosting web, web hosting, beli domain murah, beli hosting, hosting terbaik, cloud web hosting, hosting indo, cloud web hosting indonesia, web hosting terbaik, hosting domain">
 <meta name="description" content="Hosting Indonesia Terbaik uptime server 99.99%, garansi 30 hari uang kembali. Cloud Hosting Terbaik untuk website, bisnis online dan reseller domain.">
 
+<link rel="icon" href="https://qwords.com/v2/wp-content/uploads/2022/12/cropped-logo-qw-light-32x32.png" sizes="32x32" />
+<link rel="icon" href="https://qwords.com/v2/wp-content/uploads/2022/12/cropped-logo-qw-light-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon" href="https://qwords.com/v2/wp-content/uploads/2022/12/cropped-logo-qw-light-180x180.png" />
+
 <link href="{{ asset('favicon.ico') }}" rel="icon">
