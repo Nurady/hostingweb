5dad3d7a3a2e73034d6934a5f7cf4d71f8b4ff56
my office project <vueapp2022@gmail.com>
2023-12-07 13:53:51 +0800

    add footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,54 +1,132 @@
 <div class="container-fluid bg-primary text-white footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
     <div class="container py-5 px-lg-5">
-                <div class="row gy-5 gx-4 pt-5">
-                    <div class="col-12">
-                        <h5 class="fw-bold text-white mb-4">Subscribe Our Newsletter</h5>
-                        <div class="position-relative" style="max-width: 400px;">
-                            <input class="form-control bg-white border-0 w-100 py-3 ps-4 pe-5" type="text" placeholder="Enter your email">
-                            <button type="button" class="btn btn-primary py-2 px-3 position-absolute top-0 end-0 mt-2 me-2">Submit</button>
-                        </div>
-                    </div>
-                    <div class="col-lg-5 col-md-12">
-                        <div class="row gy-5 g-4">
-                            <div class="col-md-6">
-                                <h5 class="fw-bold text-white mb-4">About Us</h5>
-                                <a class="btn btn-link" href="">About Us</a>
-                                <a class="btn btn-link" href="">Contact Us</a>
-                                <a class="btn btn-link" href="">Privacy Policy</a>
-                                <a class="btn btn-link" href="">Terms & Condition</a>
-                                <a class="btn btn-link" href="">Support</a>
-                            </div>
-                            <div class="col-md-6">
-                                <h5 class="fw-bold text-white mb-4">Our Services</h5>
-                                <a class="btn btn-link" href="">Domain Register</a>
-                                <a class="btn btn-link" href="">Shared Hosting</a>
-                                <a class="btn btn-link" href="">VPS Hosting</a>
-                                <a class="btn btn-link" href="">Dedicated Hosting</a>
-                                <a class="btn btn-link" href="">Reseller Hosting</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-3">
-                        <h5 class="fw-bold text-white mb-4">Get In Touch</h5>
-                        <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-                        <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-                        <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
-                        <div class="d-flex pt-2">
-                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
-                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
-                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
-                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-4 mt-lg-n5">
-                        <div class="bg-light rounded" style="padding: 30px;">
-                            <input type="text" class="form-control border-0 py-2 mb-2" placeholder="Name">
-                            <input type="email" class="form-control border-0 py-2 mb-2" placeholder="Email">
-                            <textarea class="form-control border-0 mb-2" rows="2" placeholder="Message"></textarea>
-                            <button class="btn btn-primary w-100 py-2">Send Message</button>
-                        </div>
+        <div class="row gy-5 gx-4 pt-5">
+            <div class="row">
+                <div class="col-md-3 mb-3">
+                    <img width="190px" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/logo-dark.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/logo-dark.webp">
+
+                    <h3>PT Qwords Company International</h3>
+                    <p>Cloud Web Hosting Indonesia. Domain & hosting terbaik dengan akses cepat yang didukung layanan support 24/7</p>
+                    <img class="w-100" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/bsi.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/bsi.webp">
+
+                    <h3 class="mt-5">Customer Care</h3>
+                    <div class="d-flex flex-column mb-3">
+                        <a href="https://qwords.com/v2/order-payment/" class="text-white mb-2">Pembayaran</a>
+                        <a href="https://qwords.com/v2/permintaan-faktur-pajak/" class="text-white mb-2">Faktur Pajak</a>
+                        <a href="https://portal.qwords.com/submitticket.php?_gl=1*1wy2j6f*_gcl_au*MTc1MjQyNTI4MC4xNzAxODQzODA3&_ga=2.37488544.1021700938.1701843808-1812367761.1701843808" class="text-white mb-2">Bantuan24Jam</a>
+                        <a href="https://qwords.com/v2/qwords-rewards/" class="text-white mb-2">Qwords Rewards</a>
+                        <a href="https://kb.qwords.com/?_gl=1*wq7lh1*_gcl_au*MTc1MjQyNTI4MC4xNzAxODQzODA3&_ga=2.241240962.1021700938.1701843808-1812367761.1701843808" class="text-white mb-2">Knowledge Base</a>
+                        <a href="https://qwords.com/blog/" class="text-white mb-2">Blog</a>
+                        <a href="https://qwords.com/v2/faq/" class="text-white mb-2">FAQ</a>
+                        <a href="https://qwords.com/v2/about-qwordscom/contact-qwordscom/" class="text-white mb-2">Kontak</a>
                     </div>
                 </div>
+                <div class="col-md-3 mb-3">
+                    <h3>Mengenai Kami</h3>
+                    <div class="d-flex flex-column mb-3">
+                        <a href="https://qwords.co.id/" class="text-white mb-2">Corporate Website</a>
+                        <a href="https://qwords.com/v2/about-qwordscom/history/" class="text-white mb-2">Qwords History</a>
+                        <a href="https://qwords.com/v2/about-qwordscom/difference/" class="text-white mb-2">Qwords Differences</a>
+                        <a href="https://qwords.com/v2/media-publication/" class="text-white mb-2">Media Publication</a>
+                        <a href="https://qwords.com/v2/about-qwordscom/network/" class="text-white mb-2">Network & Infrastructure</a>
+                        <a href="https://qwords.com/v2/about-qwordscom/partner/" class="text-white mb-2">Partner</a>
+                        <a href="https://qwords.com/v2/karir" class="text-white mb-2">Career & Employment</a>
+                    </div>
+
+                    <h3 class="mt-5">MSA/SLA/AUP</h3>
+                    <div class="d-flex flex-column mb-3">
+                        <a href="https://qwords.com/v2/master-service-agreement" class="text-white mb-2">Master Service Agreement</a>
+                        <a href="https://qwords.com/v2/master-service-agreement/refund-policy/" class="text-white mb-2">Refund Policy</a>
+                        <a href="https://qwords.com/v2/master-service-agreement/privacy-policy/" class="text-white mb-2">Privacy Policy</a>
+                        <a href="https://qwords.com/v2/master-service-agreement/product-life-cycle-policy/" class="text-white mb-2">Product Life Cycle Policy</a>
+                        <a href="https://qwords.com/v2/about-qwordscom/penggunaan-merek-dan-logo/" class="text-white mb-2">Penggunaan Merek dan Logo</a>
+                        <a href="https://qwords.com/v2/master-service-agreement/migrasi-pelanggan/" class="text-white mb-2">Migrasi Ke Qwords</a>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-3">
+                    <h3>Layanan Lainnya</h3>
+                    <div class="d-flex flex-column mb-3">
+                        <a href="https://qwords.com/v2/services/extended-support/" class="text-white mb-2">Extended Support</a>
+                        <a href="https://qwords.com/v2/manage-the-box-dedicated-colocation-server/" class="text-white mb-2">Manage The Box Server</a>
+                        <a href="https://qwords.com/v2/flexi-main-domain/" class="text-white mb-2">Flexi Main Domain</a>
+                        <a href="https://qwords.com/v2/ftp-backup" class="text-white mb-2">FTP Backup</a>
+                        <a href="https://qwords.com/v2/services/offline-backup" class="text-white mb-2">Offline Backup</a>
+                        <a href="https://qwords.com/v2/services/hosting-insurance/" class="text-white mb-2">Hosting Insurance</a>
+                        <a href="https://qwords.com/v2/licenses/" class="text-white mb-2">Software License</a>
+                        <a href="https://qwords.com/v2/services/digital-signature/" class="text-white mb-2">Digital Signature</a>
+                    </div>
+
+                    <h3 class="mt-5">Layanan Reseller</h3>
+                    <div class="d-flex flex-column mb-3">
+                        <a href="https://qwords.com/v2/afiliasi/" class="text-white mb-2">Afiliasi</a>
+                        <a href="https://qwords.com/v2/qwordscom-package-reseller/" class="text-white mb-2">Qwords VIP Partner</a>
+                        <a href="https://qwords.com/v2/private-label-hosting-reseller/" class="text-white mb-2">Private Label Hosting Reseller</a>
+                        <a href="https://qwords.com/v2/private-label-domain-reseller" class="text-white mb-2">Private Label Domain Reseller</a>
+                    </div>
+
+                    <h3 class="mt-5">Free Tools</h3>
+                    <div class="d-flex flex-column mb-3">
+                        <a href="https://link.share.web.id/" class="text-white mb-2">Qwords Link-Q</a>
+                        <a href="https://wa.share.web.id/" class="text-white mb-2">WhatsApp Link Generator</a>
+                        <a href="https://qwords.info/" class="text-white mb-2">Network Tools</a>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-3">
+                    <h3>Kantor Surabaya</h3>
+                    <p>Gedung Bursa Efek Indonesia Lt.10, Jl. Taman AIS Nasution No.21, Surabaya 60271 - Indonesia 031 30008800</p>
+
+                    <h3>Kantor Yogyakarta</h3>
+                    <p>Jl. Blotan no. 18 Kayen Wedomartani, Ngemplak, Sleman, Yogyakarta 55584 - Indonesia 0274 6058800</p>
+
+                    <h3>Kantor Jakarta</h3>
+                    <p>Gedung Cyber 1 Lantai 3, Jl.Kuningan Barat no.8, Jakarta 12710 - Indonesia 021 39708800</p>
+
+                    <h3>Kantor Bandung</h3>
+                    <p>Jl. Terusan Setra Indah I No.19, Sukagalih, Sukajadi, Bandung 40163 - Indonesia 022 30508800</p>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <h3>Terdaftar di PSE, ICANN dan Partner Lainnya</h3>
+                <div class="d-flex flex-column flex-md-row flex-xl-row flex-xxl-row align-items-center mb-3">
+                    <div class="me-3">
+                        <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/code.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/code.webp">
+                    </div>
+                    <div class="me-3">
+                        <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/icann-logo.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/icann-logo.webp">
+                    </div>
+                    <div class="me-3">
+                        <img width="100px" height="100px" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/badge_litespeed.svg" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/badge_litespeed.svg">
+                    </div>
+                    <div class="me-3">
+                        <img width="100px" height="100px" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/badge_whmadmin.svg" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/badge_whmadmin.svg">
+                    </div>
+                    <div class="me-3">
+                        <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/acronis-cloud-tech.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/acronis-cloud-tech.webp">
+                    </div>
+                    <div class="me-3">
+                        <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/acronis-cloud-sales.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/acronis-cloud-sales.webp">
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="d-flex pt-2 justify-content-center">
+                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
+                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+            <div class="row mt-5 justify-content-center">
+                <div class="col-md-6">
+                    <div class="bg-light rounded" style="padding: 30px;">
+                        <input type="text" class="form-control border-0 py-2 mb-2" placeholder="Name">
+                        <input type="email" class="form-control border-0 py-2 mb-2" placeholder="Email">
+                        <textarea class="form-control border-0 mb-2" rows="2" placeholder="Message"></textarea>
+                        <button class="btn btn-primary w-100 py-2">Send Message</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="container px-lg-5">
         <div class="copyright">
@@ -56,15 +134,12 @@
                 <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
                     &copy; <a class="border-bottom" href="#">Your Site Name</a>, All Right Reserved. 
                     Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a>
-                    </br>
+                    <br>
                     Distributed By <a class="border-bottom" href="https://themewagon.com" target="_blank">ThemeWagon</a>
                 </div>
                 <div class="col-md-6 text-center text-md-end">
                     <div class="footer-menu">
-                        <a href="">Home</a>
-                        <a href="">Cookies</a>
-                        <a href="">Help</a>
-                        <a href="">FQAs</a>
+                        <p>Copyright © PT. Qwords Company International</p>
                     </div>
                 </div>
             </div>
