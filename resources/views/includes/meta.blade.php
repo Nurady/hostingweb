e5d67f0c88b4dab00574bbfc4a1651206fe96e9c
my office project <vueapp2022@gmail.com>
2023-12-07 20:05:06 +0800

    add laravel meta manager in meta html

--- resources/views/includes/meta.blade.php
+++ resources/views/includes/meta.blade.php
@@ -2,6 +2,12 @@
 
 <title>Qwords - @yield('title')</title>
 
+@include('meta::manager', [
+    'title'         => 'Qwords',
+    'description'   => 'Hosting Indonesia Terbaik uptime server 99.99%, garansi 30 hari uang kembali. Cloud Hosting Terbaik untuk website, bisnis online dan reseller domain.',
+    'image'         => 'https://qwords.com/v2/wp-content/uploads/2022/12/cropped-logo-qw-light-192x192.png',
+])
+
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
 <meta name="keywords" content="Cloud hosting terbaik, hosting cepat, sewa hosting, beli domain dan hosting, beli domain hosting, hosting, cloud hosting murah, jasa web hosting, cloud hosting, webhosting domain hosting, domain dan hosting, hosting indonesia, hosting dan domain murah, hosting domain, cloud hosting indonesia terbaik, web hosting indonesia, cloud hosting indonesia, webhost app, hosting web, web hosting, beli domain murah, beli hosting, hosting terbaik, cloud web hosting, hosting indo, cloud web hosting indonesia, web hosting terbaik, hosting domain">
 <meta name="description" content="Hosting Indonesia Terbaik uptime server 99.99%, garansi 30 hari uang kembali. Cloud Hosting Terbaik untuk website, bisnis online dan reseller domain.">
