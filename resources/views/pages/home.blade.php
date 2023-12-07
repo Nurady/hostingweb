4871625f8e19ec003e859d152d926957f5c79527
my office project <vueapp2022@gmail.com>
2023-12-07 09:29:21 +0800

    add testimonia

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -17,12 +17,12 @@
                         </p>
                     </div>
                     <div class="row text-center search-domain">
-                        <div class="col-md-8">
+                        <div class="col-md-8 mb-2">
                             <div class="form-group">
                                 <input class="form-control w-100 py-3 ps-4 pe-5" type="text" placeholder="Cari Nama Domain Anda">
                             </div>
                         </div>
-                        <div class="col-md-2">
+                        <div class="col-md-2 mb-2">
                             <div class="form-group">
                                 <select class="form-select" aria-label="Default select example">
                                     <option value="#">.id</option>
@@ -37,16 +37,16 @@
                     </div>
                     <div class="row g-3 wow fadeInUp" data-wow-delay="0.5s">
                         <div class="col-md-10 text-center">
-                            <div class="row mt-4">
-                                <div class="col-lg-4 col-md-4 col-sm-12 text-center">
+                            <div class="row mt-4 align-items-center">
+                                <div class="col-lg-4 col-md-4 col-sm-6 text-center">
                                     <p class="fw-bold text-primary mb-1 text-white">.com</p>
                                     <h4 class="mb-0 text-white">Rp. 165.000</h4>
                                 </div>
-                                <div class="col-lg-4 col-md-4 col-sm-12 text-center">
+                                <div class="col-lg-4 col-md-4 col-sm-6 text-center">
                                     <p class="fw-bold text-primary mb-1 text-white">.id</p>
                                     <h4 class="mb-0 text-white">Rp. 225.000</h4>
                                 </div>
-                                <div class="col-lg-4 col-md-4 col-sm-12 text-center">
+                                <div class="col-lg-4 col-md-4 col-sm-6 text-center">
                                     <p class="fw-bold text-primary mb-1 text-white">.info</p>
                                     <h4 class="mb-0 text-white">Rp. 225.000</h4>
                                 </div>
@@ -367,6 +367,7 @@
         </div>
     </div>
 
+    {{-- Solusi --}}
     <div class="solution container-xxl py-5">
         <div class="container px-lg-5">
             <div class="section-title position-relative text-center mx-auto mb-5 pb-4 wow fadeInUp" data-wow-delay="0.1s">
@@ -450,6 +451,7 @@
         </div>
     </div>
 
+    {{-- Komitmen --}}
     <div class="commitment container-xxl py-5">
         <div class="container px-lg-5">
             <div class="section-title position-relative text-center mx-auto mb-5 pb-4 wow fadeInUp" data-wow-delay="0.1s">
@@ -557,7 +559,7 @@
         </div>
     </div>
 
-    {{-- Garansi --}}
+    {{-- Pelanggan Aktif --}}
     <div class="commitment container-xxl py-5">
         <div class="container px-lg-5">
             <div class="position-relative text-center mx-auto mb-3 pb-4 wow fadeInUp" data-wow-delay="0.1s">
@@ -566,22 +568,22 @@
                 </h1>
             </div>
             <div class="row wow fadeInUp">
-                <div class="col-md-2 mb-2">
+                <div class="col-md-2 col-sm-6 mb-2">
                     <img class="w-100" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/partner-customer/Pelanggan/Cimory.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/partner-customer/Pelanggan/Cimory.webp">
                 </div>
-                <div class="col-md-2 mb-2">
+                <div class="col-md-2 col-sm-6 mb-2">
                     <img class="w-100" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/partner-customer/Pelanggan/Forisa.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/partner-customer/Pelanggan/Forisa.webp">
                 </div>
-                <div class="col-md-2 mb-2">
+                <div class="col-md-2 col-sm-6 mb-2">
                     <img class="w-100" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/partner-customer/Pelanggan/Elsheskin.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/partner-customer/Pelanggan/Elsheskin.webp">
                 </div>
-                <div class="col-md-2 mb-2">
+                <div class="col-md-2 col-sm-6 mb-2">
                     <img class="w-100" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/partner-customer/Pelanggan/Icon.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/partner-customer/Pelanggan/Icon.webp">
                 </div>
-                <div class="col-md-2 mb-2">
+                <div class="col-md-2 col-sm-6 mb-2">
                     <img class="w-100" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/partner-customer/Pelanggan/Horison.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/partner-customer/Pelanggan/Horison.webp">
                 </div>
-                <div class="col-md-2 mb-2">
+                <div class="col-md-2 col-sm-6 mb-2">
                     <img class="w-100" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/partner-customer/Pelanggan/MD.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/partner-customer/Pelanggan/MD.webp">
                 </div>
             </div>
@@ -638,6 +640,105 @@
                 <div class="col-md-6">
                     <h3>Garansi 30 Hari <span>Uang Kembali</span></h3>
                     <p>Untuk menjaga kepuasan pelanggan, kami memberikan garansi uang kembali yang berlaku hingga 30 hari setelah hosting aktif.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- Testimonial --}}
+    <div class="testimonial container-xxl py-5 mt-5">
+        <div class="container px-lg-5">
+            <div class="section-title position-relative text-center mx-auto mb-5 pb-4 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="row justify-content-center">
+                    <div class="col-md-7">
+                        <h1 class="mb-3">
+                            Apa Kata Mereka?
+                        </h1>
+                        <p class="mb-1 mx-auto">
+                            Apa kata mereka yang sudah menggunakan layanan web hosting Indonesia dari Qwords? Simak pengalaman mereka yang telah membuktikan sendiri fitur dari layanan kami. 
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex flex-column-reverse flex-md-row flex-xl-row flex-xxl-row align-items-center mb-3">
+                <div class="p-2 w-50 quote">                    
+                    <div class="header-quote">
+                        <h5>
+                            Semoga Qwords dapat terus bertahan, sehingga kami dan masyarakat Indonesia bisa mendapatkan layanan yang terbaik dari Qwords.
+                        </h5>
+                    </div>
+                    <div class="quote-details fst-italic">
+                        Kami, ID Cert, merupakan sebuah perusahaan yang bergerak dalam bidang penyedia jasa informasi keamanan di Indonesia. Oleh karena itu, kami membutuhkan Qwords untuk mendukung kebutuhan kami terhadap server dan hosting yang selalu up selama 24 jam.
+                    </div>
+                    <div class="author-name ms-5 pt-3 pb-2">
+                        <h6 class="d-inline me-3">Budi Raharjo</h6>
+                        <span class="small text-muted">Founder ID Cert</span>
+                    </div>
+                    <div class="ms-5">
+                        <a class="other" href="https://www.youtube.com/watch?v=q2bJogin3eE&feature=youtu.be">
+                            Lihat Video Selengkapnya
+                        </a>
+                        <span>
+                            <img style="height: 20px !important; width: 20px !important;" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/Right.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/Right.webp">
+                        </span>
+                    </div>                    
+                </div>
+                <div class="p-2 w-50 quote-profil">
+                    <img class="w-100" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/testi1.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/testi1.webp">
+                </div>
+            </div>
+            <div class="d-flex flex-column flex-md-row flex-xl-row flex-xxl-row align-items-center mb-3">
+                <div class="p-2 w-50 quote-profil">
+                    <img class="w-100" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/testi2.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/testi2.webp">
+                </div>
+                <div class="p-2 w-50 quote">                    
+                    <div class="header-quote">
+                        <h5>
+                            Pelayanan Qwords sangat cepat dan responsif, sehingga tidak butuh waktu lama untuk membantu website kami kembali normal. Terbaik pokoknya, mah!.
+                        </h5>
+                    </div>
+                    <div class="quote-details fst-italic">
+                        Sebagai media penyedia kabar seputar kota Bandung, Info BDG menggunakan website sebagai rumah informasi. Website kami yang sempat down kini menunjukkan traffic yang tinggi setelah menggunakan dukungan dari Qwords.
+                    </div>
+                    <div class="author-name ms-5 pt-3 pb-2">
+                        <h6 class="d-inline me-3">Kuns Kurniawan</h6>
+                        <span class="small text-muted"> PR Event Info BDG</span>
+                    </div>
+                    <div class="ms-5">
+                        <a class="other" href="https://www.youtube.com/watch?v=f15zt4C2e4A&feature=youtu.be">
+                            Lihat Video Selengkapnya
+                        </a>
+                        <span>
+                            <img style="height: 20px !important; width: 20px !important;" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/Right.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/Right.webp">
+                        </span>
+                    </div>                    
+                </div>
+            </div>
+            <div class="d-flex flex-column-reverse flex-md-row flex-xl-row flex-xxl-row align-items-center mb-3">
+                <div class="p-2 w-50 quote">                    
+                    <div class="header-quote">
+                        <h5>
+                            Hingga saat ini pula, kami tidak pernah menemukan masalah yang berarti dari layanan yang diberikan Qwords. Layanan Customer Service yang sangat baik, membuat kami tidak ingin berpindah dari Qwords.
+                        </h5>
+                    </div>
+                    <div class="quote-details fst-italic">
+                        Penjualan Rumah Kopi Temanggung 90% dilakukan secara online, salah satunya menggunakan website. Semenjak tahun 2015 hingga saat ini, kami menggunakan hosting dan domain dari Qwords.
+                    </div>
+                    <div class="author-name ms-5 pt-3 pb-2">
+                        <h6 class="d-inline me-3">Denden Sofiudin</h6>
+                        <span class="small text-muted">Owner Rumah Kopi temanggung</span>
+                    </div>
+                    <div class="ms-5">
+                        <a class="other" href="https://www.youtube.com/watch?v=ftJMF63RznM&feature=youtu.be">
+                            Lihat Video Selengkapnya
+                        </a>
+                        <span>
+                            <img style="height: 20px !important; width: 20px !important;" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/Right.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/Right.webp">
+                        </span>
+                    </div>                    
+                </div>
+                <div class="p-2 w-50 quote-profil">
+                    <img class="w-100" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/testi3.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/testi3.webp">
                 </div>
             </div>
         </div>
