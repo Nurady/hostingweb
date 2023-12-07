a77040e4241c88db4b7356ef089a9468a9c63a29
my office project <vueapp2022@gmail.com>
2023-12-07 19:55:38 +0800

    update meta keywords dan descriptions

--- resources/views/includes/meta.blade.php
+++ resources/views/includes/meta.blade.php
@@ -1,7 +1,9 @@
 <meta charset="utf-8">
+
 <title>Qwords - @yield('title')</title>
+
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
-<meta content="" name="keywords">
-<meta content="" name="description">
+<meta name="keywords" content="Cloud hosting terbaik, hosting cepat, sewa hosting, beli domain dan hosting, beli domain hosting, hosting, cloud hosting murah, jasa web hosting, cloud hosting, webhosting domain hosting, domain dan hosting, hosting indonesia, hosting dan domain murah, hosting domain, cloud hosting indonesia terbaik, web hosting indonesia, cloud hosting indonesia, webhost app, hosting web, web hosting, beli domain murah, beli hosting, hosting terbaik, cloud web hosting, hosting indo, cloud web hosting indonesia, web hosting terbaik, hosting domain">
+<meta name="description" content="Hosting Indonesia Terbaik uptime server 99.99%, garansi 30 hari uang kembali. Cloud Hosting Terbaik untuk website, bisnis online dan reseller domain.">
 
 <link href="{{ asset('favicon.ico') }}" rel="icon">
