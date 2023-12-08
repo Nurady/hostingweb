b9f1b278e271039d43db0c6892d2462271abfc71
my office project <vueapp2022@gmail.com>
2023-12-08 19:33:12 +0800

    add css animation to store image promo

--- resources/views/components/promo.blade.php
+++ resources/views/components/promo.blade.php
@@ -3,7 +3,7 @@
         <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/illustrations/promo-sign.webp" class="promo-sign entered litespeed-loaded" alt="promo-sign.webp">
     </div>
     <div class="container-promo"> 
-            <img class="imgpromo" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/illustrations/promo-text.webp" class="promo-text entered litespeed-loaded" alt="cek diskon hosting sekarang!">
+        <img class="imgpromo" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/illustrations/promo-text.webp" class="promo-text entered litespeed-loaded" alt="cek diskon hosting sekarang!">
         <img class="imgstoresection" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/illustrations/storesection.webp" class="promo-illustration entered litespeed-loaded" alt="cek promo hosting Indonesia terbaru">
     </div>
     <div id="close" onclick="hideAndShowDiv()" class="me-5 btn" style="margin-top: 80px; color: #CF3B21; font-weight: 800;">Close X</div>
