aa7b158f124b38b5a90142ff7649e14885b46cd1
my office project <vueapp2022@gmail.com>
2023-12-08 09:13:33 +0800

    add component promo dan hepldesk

---
+++ resources/views/components/helpdesk.blade.php
@@ -0,0 +1,1 @@
+<div class="BottomButton_icon__Yjla4"></div>
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -18,19 +18,9 @@
         <!-- Back to Top -->
         <a href="#" class="btn btn-lg btn-secondary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
 
-         <!-- Kode Tambahan -->
-        <div class="BottomButton_icon__Yjla4"></div>
-
-        <div class="container-promo-img">
-            <div id="promo-sign">
-                <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/illustrations/promo-sign.webp" class="promo-sign entered litespeed-loaded" alt="promo-sign.webp">
-            </div>
-            <div class="container-promo"> 
-                    <img class="imgpromo" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/illustrations/promo-text.webp" class="promo-text entered litespeed-loaded" alt="cek diskon hosting sekarang!">
-                <img class="imgstoresection" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/illustrations/storesection.webp" class="promo-illustration entered litespeed-loaded" alt="cek promo hosting Indonesia terbaru">
-            </div>
-            <div id="close" onclick="hideAndShowDiv()" class="me-5 btn" style="margin-top: 80px; color: #CF3B21; font-weight: 800;">Close X</div>
-        </div>
+        <!-- Kode Tambahan -->
+        <x-helpdesk/>
+        <x-promo/>
     </div>
 
     @include('includes.script') 
