eeb85afb4772b9e2b2ea76f2f5c020ecf8a7ed9c
my office project <vueapp2022@gmail.com>
2023-12-11 21:55:46 +0800

    equal height card other service

--- resources/views/components/other-service.blade.php
+++ resources/views/components/other-service.blade.php
@@ -8,79 +8,91 @@
                 Optimalkan performa website bisnis Anda dengan berbagai layanan berkualitas dari Qwords.
             </p>
         </div>
-        <div class="row gy-5 gx-4">
-            <div class="service-other-card col-md-4 wow fadeInUp" data-wow-delay="0.2s">
-                <div class="position-relative shadow rounded border-top border-5">
-                    <div class="text-center border-bottom p-2">                        
-                        <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/smallicons/ssl.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/smallicons/ssl.webp">                    
-                        <h4 class="fw-bold mt-2">SSL</h4>
-                    </div>
-                    <div class="text-center border-bottom p-4">
-                        <p class="text-muted mb-1">Jadikan website Anda lebih terpercaya dan disukai oleh search engine dengan menggunakan SSL Certificate.</p>
-                        <p class="mb-3 mt-3">
-                            Mulai Dari
-                        </p>
-                        <h3>
-                            <span style="color: #ff6d0b; ">
-                                Rp. 112.000
-                            </span>
-                            <span class="text-muted" style="font-size: 20px !important;">
-                                /tahun
-                            </span>
-                        </h3>
-                        <a class="btn btn-primary px-4 py-2 mt-4 w-100" href="https://qwords.com/v2/secured-sockets-layer-certificate/?_gl=1%2A87kryz%2A_gcl_au%2AMTc1MjQyNTI4MC4xNzAxODQzODA3&_ga=2.2238128.1021700938.1701843808-1812367761.1701843808">
-                            Pesan Sekarang
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="service-other-card col-md-4 wow fadeInUp" data-wow-delay="0.2s">
-                <div class="position-relative shadow rounded border-top border-5">
-                    <div class="text-center border-bottom p-2">                        
-                        <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/smallicons/dbstack.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/smallicons/dbstack.webp">                    
-                        <h4 class="fw-bold mt-2">Dedicated Server</h4>
-                    </div>
-                    <div class="text-center border-bottom p-4">
-                        <p class="text-muted mb-1">Nikmati kelola Dedicated Server sendiri dengan akses remote dari seluruh dunia tanpa batas.</p>
-                        <p class="mb-3 mt-3">
-                            Mulai Dari
-                        </p>
-                        <h3>
-                            <span style="color: #ff6d0b; ">
-                                Rp. 499.000
-                            </span>
-                            <span class="text-muted" style="font-size: 20px !important;">
-                                /tahun
-                            </span>
-                        </h3>
-                        <a class="btn btn-primary px-4 py-2 mt-4 w-100" href="https://qwords.com/v2/dedicated-server/dedicated-box/?_gl=1*13zm0s9*_gcl_au*MTc1MjQyNTI4MC4xNzAxODQzODA3&_ga=2.6778802.1021700938.1701843808-1812367761.1701843808">
-                            Pesan Sekarang
-                        </a>
+        <div class="row row-cols-1 row-cols-md-3 gy-5 gx-4">
+            <div class="col">
+                <div class="card h-100 wow fadeInUp" data-wow-delay="0.2s">
+                    <div class="card-body p-0">
+                        <div class="position-relative h-100 shadow rounded border-top border-5">
+                            <div class="text-center border-bottom p-2">                        
+                                <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/smallicons/ssl.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/smallicons/ssl.webp">                    
+                                <h4 class="fw-bold mt-2">SSL</h4>
+                            </div>
+                            <div class="text-center p-4">
+                                <p class="text-muted mb-1">Jadikan website Anda lebih terpercaya dan disukai oleh search engine dengan menggunakan SSL Certificate.</p>
+                                <p class="mb-3 mt-3">
+                                    Mulai Dari
+                                </p>
+                                <h3>
+                                    <span style="color: #ff6d0b; ">
+                                        Rp. 112.000
+                                    </span>
+                                    <span class="text-muted" style="font-size: 20px !important;">
+                                        /tahun
+                                    </span>
+                                </h3>
+                                <a class="btn btn-primary px-4 py-2 mt-4 w-100" href="https://qwords.com/v2/secured-sockets-layer-certificate/?_gl=1%2A87kryz%2A_gcl_au%2AMTc1MjQyNTI4MC4xNzAxODQzODA3&_ga=2.2238128.1021700938.1701843808-1812367761.1701843808">
+                                    Pesan Sekarang
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="service-other-card col-md-4 wow fadeInUp" data-wow-delay="0.2s">
-                <div class="position-relative shadow rounded border-top border-5">
-                    <div class="text-center border-bottom p-2">                        
-                        <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/smallicons/disk.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/smallicons/disk.webp">                    
-                        <h4 class="fw-bold mt-2">Colocation Server</h4>
+            <div class="col">
+                <div class="card h-100 wow fadeInUp" data-wow-delay="0.2s">
+                    <div class="card-body p-0">
+                        <div class="position-relative h-100 shadow rounded border-top border-5">
+                            <div class="text-center border-bottom p-2">                        
+                                <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/smallicons/dbstack.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/smallicons/dbstack.webp">                    
+                                <h4 class="fw-bold mt-2">Dedicated Server</h4>
+                            </div>
+                            <div class="text-center p-4">
+                                <p class="text-muted mb-1">Nikmati kelola Dedicated Server sendiri dengan akses remote dari seluruh dunia tanpa batas.</p>
+                                <p class="mb-3 mt-3">
+                                    Mulai Dari
+                                </p>
+                                <h3>
+                                    <span style="color: #ff6d0b; ">
+                                        Rp. 499.000
+                                    </span>
+                                    <span class="text-muted" style="font-size: 20px !important;">
+                                        /tahun
+                                    </span>
+                                </h3>
+                                <a class="btn btn-primary px-4 py-2 mt-4 w-100" href="https://qwords.com/v2/dedicated-server/dedicated-box/?_gl=1*13zm0s9*_gcl_au*MTc1MjQyNTI4MC4xNzAxODQzODA3&_ga=2.6778802.1021700938.1701843808-1812367761.1701843808">
+                                    Pesan Sekarang
+                                </a>
+                            </div>
+                        </div>
                     </div>
-                    <div class="text-center border-bottom p-3">
-                        <p class="text-muted mb-1">Akses cepat dari seluruh dunia dengan fasilitas 24x7 full monitoring dengan cooling system terbaik serta fire extinguisher.</p>
-                        <p class="mb-3 mt-3">
-                            Mulai Dari
-                        </p>
-                        <h3>
-                            <span style="color: #ff6d0b; ">
-                                Rp. 500.000
-                            </span>
-                            <span class="text-muted" style="font-size: 20px !important;">
-                                /tahun
-                            </span>
-                        </h3>
-                        <a class="btn btn-primary px-4 py-2 mt-3 w-100" href="https://qwords.com/v2/dedicated-server/colocation-server/?_gl=1*13zm0s9*_gcl_au*MTc1MjQyNTI4MC4xNzAxODQzODA3&_ga=2.6778802.1021700938.1701843808-1812367761.1701843808">
-                            Pesan Sekarang
-                        </a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100 wow fadeInUp" data-wow-delay="0.2s">
+                    <div class="card-body p-0">
+                        <div class="position-relative h-100 shadow rounded border-top border-5">
+                            <div class="text-center border-bottom p-2">                        
+                                <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/smallicons/disk.webp" alt="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/new-icon/smallicons/disk.webp">                    
+                                <h4 class="fw-bold mt-2">Colocation Server</h4>
+                            </div>
+                            <div class="text-center p-4">
+                                <p class="text-muted mb-1">Akses cepat dari seluruh dunia dengan fasilitas 24x7 full monitoring dengan cooling system terbaik serta fire extinguisher.</p>
+                                <p class="mb-3 mt-3">
+                                    Mulai Dari
+                                </p>
+                                <h3>
+                                    <span style="color: #ff6d0b; ">
+                                        Rp. 500.000
+                                    </span>
+                                    <span class="text-muted" style="font-size: 20px !important;">
+                                        /tahun
+                                    </span>
+                                </h3>
+                                <a class="btn btn-primary px-4 py-2 mt-4 w-100" href="https://qwords.com/v2/dedicated-server/colocation-server/?_gl=1*13zm0s9*_gcl_au*MTc1MjQyNTI4MC4xNzAxODQzODA3&_ga=2.6778802.1021700938.1701843808-1812367761.1701843808">
+                                    Pesan Sekarang
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
