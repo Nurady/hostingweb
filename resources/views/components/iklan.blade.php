05a6675a9566f054220d1d513caa56d97728f236
my office project <vueapp2022@gmail.com>
2023-12-29 13:46:37 +0800

    remove unused button in iklan

--- resources/views/components/iklan.blade.php
+++ resources/views/components/iklan.blade.php
@@ -8,9 +8,7 @@
                 <div class="col-md-8 mb-2">
                     <h3><span>Bikin website impian</span> dengan mudah disini hanya 1jutaan</h3>
                     <p>Bikin website impian include paket hosting Indonesia terbaik hanya 1 jutaan Jasa pembuatan website profesional untuk Perusahaan, Bisnis & UKM. Bikin Website Impian dengan mudah di sini, desain website elegan dan mudah dikelola</p>
-                    <a href="https://bikin.website/" class="btn btn-primary">
-                        Lihat Selengkapnya
-                    </a>
+                    <x-button.button-order class="btn btn-primary" url="https://bikin.website/" title="Lihat Selengkapnya" />
                 </div>
             </div>
         </div>
